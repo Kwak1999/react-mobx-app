f29f59bfb3e49ebeee971113c64303e709c09d69
HuiBeom <gmlqja7913@daum.net>
2025-09-20 16:12:52 +0900

    mobx 사용해보기 counter 앱 만들기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CounterProvider } from './context/counterContext';
+import CounterStore from './CountStore';
+
+const store = new CounterStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <CounterProvider value={store}>
+            <App />
+        </CounterProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
